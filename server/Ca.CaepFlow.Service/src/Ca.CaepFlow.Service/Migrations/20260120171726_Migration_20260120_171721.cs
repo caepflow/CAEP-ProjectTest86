c39e70f3edefb9c04442ef8e7f1e872efe0493bb
streampulse <streampulse@noreply.com>
2026-01-20 17:17:26 +0000

    chore: Add database migrations for project 2deb6965-e0f9-4eec-a2d6-02a3c69cd646

---
+++ server/Ca.CaepFlow.Service/src/Ca.CaepFlow.Service/Migrations/20260120171726_Migration_20260120_171721.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ca.CaepFlow.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_20260120_171721 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NewEntities");
+
+            migrationBuilder.CreateTable(
+                name: "TestiamoDiNuovo4S",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestiamoDiNuovo4S", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestiamoDiNuovo4S");
+
+            migrationBuilder.CreateTable(
+                name: "NewEntities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewEntities", x => x.Id);
+                });
+        }
+    }
+}
