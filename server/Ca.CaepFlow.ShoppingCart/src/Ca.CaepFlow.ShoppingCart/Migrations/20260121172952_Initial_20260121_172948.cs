34fd46c49def79123629bcda6f50f41cce0c48e3
streampulse <streampulse@noreply.com>
2026-01-21 17:29:52 +0000

    chore: Add database migrations for project 2deb6965-e0f9-4eec-a2d6-02a3c69cd646

---
+++ server/Ca.CaepFlow.ShoppingCart/src/Ca.CaepFlow.ShoppingCart/Migrations/20260121172952_Initial_20260121_172948.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ca.CaepFlow.ShoppingCart.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_20260121_172948 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Utente2S",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Qualcosa = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utente2S", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Utente2S");
+        }
+    }
+}
