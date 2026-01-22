aa6c43d7f630a6b2a7ee059c59dd43df71080727
streampulse <streampulse@noreply.com>
2026-01-22 09:24:36 +0000

    chore: Add database migrations for project 2deb6965-e0f9-4eec-a2d6-02a3c69cd646

---
+++ server/Ca.CaepFlow.Service/src/Ca.CaepFlow.Service/Migrations/20260122092436_Migration_20260122_092432.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ca.CaepFlow.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_20260122_092432 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "TestiamoDiNuovo4S",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "TestiamoDiNuovo4S");
+        }
+    }
+}
