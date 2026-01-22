472ecef1da885f55dcdb960c0e7f289c95ca04cd
streampulse <streampulse@noreply.com>
2026-01-22 14:29:42 +0000

    chore: Add database migrations for project 2deb6965-e0f9-4eec-a2d6-02a3c69cd646

--- server/Ca.CaepFlow.ShoppingCart/src/Ca.CaepFlow.ShoppingCart/Migrations/20260122142942_Migration_20260122_142938.Designer.cs
+++ server/Ca.CaepFlow.ShoppingCart/src/Ca.CaepFlow.ShoppingCart/Migrations/20260122142942_Migration_20260122_142938.Designer.cs
@@ -4,6 +4,7 @@ using Ca.CaepFlow.ShoppingCart.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ca.CaepFlow.ShoppingCart.Migrations
 {
     [DbContext(typeof(ShoppingCartDataContext))]
-    partial class ShoppingCartDataContextModelSnapshot : ModelSnapshot
+    [Migration("20260122142942_Migration_20260122_142938")]
+    partial class Migration_20260122_142938
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/Ca.CaepFlow.ShoppingCart/src/Ca.CaepFlow.ShoppingCart/Migrations/20260122142942_Migration_20260122_142938.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ca.CaepFlow.ShoppingCart.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_20260122_142938 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Utente2S");
+
+            migrationBuilder.CreateTable(
+                name: "Utente3S",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Qualcosa = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utente3S", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Utente3S");
+
+            migrationBuilder.CreateTable(
+                name: "Utente2S",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Qualcosa = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utente2S", x => x.Id);
+                });
+        }
+    }
+}
