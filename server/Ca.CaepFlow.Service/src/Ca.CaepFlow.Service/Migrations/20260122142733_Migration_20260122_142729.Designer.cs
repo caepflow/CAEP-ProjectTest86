f22566213976440af999683a58dd924ad6a2ae17
streampulse <streampulse@noreply.com>
2026-01-22 14:27:33 +0000

    chore: Add database migrations for project 2deb6965-e0f9-4eec-a2d6-02a3c69cd646

--- server/Ca.CaepFlow.Service/src/Ca.CaepFlow.Service/Migrations/20260122142733_Migration_20260122_142729.Designer.cs
+++ server/Ca.CaepFlow.Service/src/Ca.CaepFlow.Service/Migrations/20260122142733_Migration_20260122_142729.Designer.cs
@@ -4,6 +4,7 @@ using Ca.CaepFlow.Service.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ca.CaepFlow.Service.Migrations
 {
     [DbContext(typeof(NewServiceDataContext))]
-    partial class NewServiceDataContextModelSnapshot : ModelSnapshot
+    [Migration("20260122142733_Migration_20260122_142729")]
+    partial class Migration_20260122_142729
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/Ca.CaepFlow.Service/src/Ca.CaepFlow.Service/Migrations/20260122142733_Migration_20260122_142729.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ca.CaepFlow.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_20260122_142729 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestiamoDiNuovo5S");
+
+            migrationBuilder.CreateTable(
+                name: "TestiamoDiNuovo6S",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestiamoDiNuovo6S", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestiamoDiNuovo6S");
+
+            migrationBuilder.CreateTable(
+                name: "TestiamoDiNuovo5S",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestiamoDiNuovo5S", x => x.Id);
+                });
+        }
+    }
+}
