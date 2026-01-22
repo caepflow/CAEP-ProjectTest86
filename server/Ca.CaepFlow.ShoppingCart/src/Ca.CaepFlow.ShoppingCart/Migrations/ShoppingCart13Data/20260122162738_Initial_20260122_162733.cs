e24fd48df3f50be8f0a14b81b5ddc2ce8d062ddd
streampulse <streampulse@noreply.com>
2026-01-22 16:27:38 +0000

    chore: Add database migrations for project 2deb6965-e0f9-4eec-a2d6-02a3c69cd646

---
+++ server/Ca.CaepFlow.ShoppingCart/src/Ca.CaepFlow.ShoppingCart/Migrations/ShoppingCart13Data/20260122162738_Initial_20260122_162733.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ca.CaepFlow.ShoppingCart.Migrations.ShoppingCart13Data
+{
+    /// <inheritdoc />
+    public partial class Initial_20260122_162733 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prodottos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prodottos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Utente3S",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Qualcosa = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utente3S", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prodottos");
+
+            migrationBuilder.DropTable(
+                name: "Utente3S");
+        }
+    }
+}
